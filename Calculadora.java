bd2720beca2b1ab651073a1cd71096cc6a105bc4
roydanpe <rapazaan@unsa.edu.pe>
2023-07-28 01:43:27 -0500

    prueba 2 , interfaz

--- Calculadora.java
+++ Calculadora.java
@@ -2,45 +2,55 @@ import java.util.*;
 class Calculadora{
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Ingrese que operacion desea utilizar");
-		String[] operaciones = {"1:Suma","2:Resta","3:Multiplicacion","4:Division","5:Raiz","6:Potencia","7:Modulo"};
-		for(String operacion:operaciones){
+                while(true){
+                    	System.out.println("Ingrese que operacion desea utilizar");
+			String[] operaciones = {"1:Suma","2:Resta","3:Multiplicacion","4:Division","5:Raiz","6:Potencia","7:Modulo"};
+			for(String operacion:operaciones){
 			System.out.println(operacion);
-		}
-		int op = sc.nextInt();
-		System.out.println("Ingrese dos numeros");
-		double n1 = sc.nextDouble();
-		double n2 = sc.nextDouble();
-		switch (op){
-			case 1:
-				double sum = suma(n1,n2);
-				System.out.println("Suma: "+sum);
-				break;
-			case 2:
-				double rest = resta(n1,n2);
-				System.out.println("Resta: "+rest);
-				break;
-			case 3:
-				double mult = multiplicacion(n1,n2);
-				System.out.println("Multiplicacion: "+mult);
-				break;
-			case 4:
-				System.out.print("Division: ");
-				division(n1,n2);
-				break;
-			case 5:
-			    System.out.print("Raiz: ");
-				raiz(n1,n2);
-				break;
-			case 6:
-				System.out.print("Potencia: ");
-				potencia(n1,n2);
-				break;
-			case 7:
-				System.out.print("Modulo: ");
-				modulo((int)n1,(int)(n2));
-				break;
-		}
+			}
+			int op = sc.nextInt();
+			System.out.println("Ingrese dos numeros");
+			double n1 = sc.nextDouble();
+			double n2 = sc.nextDouble();
+			switch (op){
+				case 1:
+					double sum = suma(n1,n2);
+					System.out.println("Suma: "+sum);
+					break;
+				case 2:
+					double rest = resta(n1,n2);
+					System.out.println("Resta: "+rest);
+					break;
+				case 3:
+					double mult = multiplicacion(n1,n2);
+					System.out.println("Multiplicacion: "+mult);
+					break;
+				case 4:
+					System.out.print("Division: ");
+					division(n1,n2);
+					break;
+				case 5:
+			    		System.out.print("Raiz: ");
+					raiz(n1,n2);
+					break;
+				case 6:
+					System.out.print("Potencia: ");
+					potencia(n1,n2);
+					break;
+				case 7:
+					System.out.print("Modulo: ");
+					modulo((int)n1,(int)(n2));
+					break;
+			}
+                        System.out.println("¿Desea hacer otra operacion? (si/no)");
+                        String rpta = sc.next();
+                        if(rpta.equalsIgnoreCase("si")){
+                            break;
+                        }
+
+                   
+                }
+		
 	
 	}
 	public static void raiz( double n, double a){
